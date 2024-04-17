c9e0f4335df1a2e270e3c8ef5af8adaa1c46994b
Thais Martins <thaismartins1999@gmail.com>
2024-04-16 21:55:59 -0300

    feat: enable cors

--- app/init.py
+++ app/init.py
@@ -1,14 +1,17 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from routes import init_routes
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    app.config['FLASK_APP'] = os.environ.get('FLASK_APP')
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    app.config["FLASK_APP"] = os.environ.get("FLASK_APP")
+    app.config["CORS_HEADERS"] = "Content-Type"
 
     init_routes(app)
 
