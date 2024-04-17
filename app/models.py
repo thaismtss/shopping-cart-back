75ac18ea8ae44134a95a96e06bbd173b04b4fba4
Thais Martins <thaismartins1999@gmail.com>
2024-04-16 21:56:32 -0300

    feat: add colum title to table cartItem

--- app/models.py
+++ app/models.py
@@ -4,21 +4,24 @@ from models import *
 
 db = SQLAlchemy()
 
+
 class Cart(db.Model):
-    __tablename__ = 'cart'
+    __tablename__ = "cart"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
+
 class CartItem(db.Model):
-    __tablename__ = 'cart_item'
+    __tablename__ = "cart_item"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'))
+    cart_id = db.Column(db.Integer, db.ForeignKey("cart.id"))
     product_id = db.Column(db.Integer)
     quantity = db.Column(db.Integer, default=1)
     image_url = db.Column(db.String(255))
     price = db.Column(db.Float)
+    title = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
